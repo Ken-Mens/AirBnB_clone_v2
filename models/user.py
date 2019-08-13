24b963ccc958b0e55c9e31a6cd6173baead0f85b
JohnCook17 <655@holbertonschool.com>
2019-08-13 12:46:54 -0700

    reverted to old code for now

--- models/user.py
+++ models/user.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This is the class for user
     Attributes:
         email: email address
@@ -14,9 +11,7 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""

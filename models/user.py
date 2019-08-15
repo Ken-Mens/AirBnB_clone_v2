4bdb8db87dd3fdc5e9ccffe7c1b4b5076ac7404f
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-15 00:26:06 +0000

    add user model

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+
+
 class User(BaseModel, Base):
     """This is the class for user
     Attributes:

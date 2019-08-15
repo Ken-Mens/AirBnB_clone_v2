eda908bb149c6a461e9ce45365e01e20930d5935
JohnCook17 <655@holbertonschool.com>
2019-08-14 18:58:34 -0700

    fixed a typo in review

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,6 +6,7 @@ from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.state import State
+from models.review import Review
 from models.user import User
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey

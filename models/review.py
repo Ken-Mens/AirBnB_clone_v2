b35e5bc492dd50a4164a26026fbd059d1e065974
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-15 19:13:42 +0000

    fxied syntax

--- models/review.py
+++ models/review.py
@@ -18,7 +18,7 @@ class Review(BaseModel, Base):
                       ForeignKey('places.id'),
                       nullable=False)
     user_id = Column(String(60),
-                     ForeignKey('users.id')
+                     ForeignKey('users.id'),
                      nullable=False)
     text = Column(String(1024),
                   nullable=False)

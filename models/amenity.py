57eb6560d49a15329530ee0ce64c15d32775d841
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-15 20:24:07 +0000

    update amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -3,9 +3,10 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name

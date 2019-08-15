66bf2d81ae338930d7cb78949b78552f355ba617
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-15 19:01:44 +0000

    add review class

--- models/review.py
+++ models/review.py
@@ -14,14 +14,11 @@ class Review(BaseModel, Base):
         user_id: user id
         text: review description
     """
-    __tablename__ = "reviews"
     place_id = Column(String(60),
                       ForeignKey('places.id'),
                       nullable=False)
     user_id = Column(String(60),
-                     ForeignKey('users.id'),
+                     ForeignKey('users.id')
                      nullable=False)
     text = Column(String(1024),
                   nullable=False)
-
-    place = relationship("Place")

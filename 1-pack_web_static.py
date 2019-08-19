a5e3320cc71522a76e28c88df7cec1b1c819c1fb
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-19 21:24:33 +0000

    compress before sending where I write a fabric script that generates a .tgz archive from web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Fabric script that generates a .tgz archive
+from fabric.api import *
+from datetime import datetime, date
+
+def do_pack():
+    """creates a .tgz file
+    """
+    time_var = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    path = 'versions/web_static_{}.tgz'.format(time_var)
+    try:
+        local("mkdir -p ./versions")
+        local("tar -czvf {} ./web_static".format(path))
+        return path
+    except:
+        return None

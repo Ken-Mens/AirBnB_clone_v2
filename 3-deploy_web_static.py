47c44c23e9aabca3e4950c4ea3b8ffc2dd535fc5
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-20 05:57:56 +0000

    added web deployment task

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -25,31 +25,35 @@ def do_pack():
 
 def do_deploy(archive_path):
     """
-    deploy
+    deploy archives
     """
     if not os.path.exists(archive_path):
         return False
 
-    filename = archive_path.split("/")[-1]
-    put(archive_path, "/tmp/{}".format(filename))
+    try:
+        filename = archive_path.split("/")[-1]
+        put(archive_path, "/tmp/{}".format(filename))
 
-    run("mkdir -p /data/web_static/releases/{}".format(filename))
+        run("mkdir -p /data/web_static/releases/{}".format(filename))
 
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-        .format(filename, filename))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+            .format(filename, filename))
 
-    run("rm /tmp/{}".format(filename))
+        run("rm /tmp/{}".format(filename))
 
-    run("mv /data/web_static/releases/{}/web_static/*"
-        " /data/web_static/releases/{}"
-        .format(filename, filename))
+        run("mv /data/web_static/releases/{}/web_static/*"
+            " /data/web_static/releases/{}"
+            .format(filename, filename))
 
-    run("rm -rf /data/web_static/releases/{}/web_static"
-        .format(filename))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(filename))
 
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
+        return True
+    except:
+        return False
 
     def deploy():
         try:

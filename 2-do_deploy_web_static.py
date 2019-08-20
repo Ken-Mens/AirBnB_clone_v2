a764568eed124b9924af37b6c2725e0cc233584e
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-20 02:31:40 +0000

    deploy archives

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Deploy archive""" 
+import os
+from fabric.api import *
+env.hosts = ['34.73.28.199']
+
+
+def do_deploy(archive_path):
+    if not os.path.exists(archive_path):
+        return False
+
+    filename = archive_path.split("/")[-1]
+    put(archive_path, "/tmp/{}".format(filename))
+
+    try:
+        run("mkdir -p /data/web_static/releases/{}".format(filename))
+
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+        .format(filename, filename))
+       
+        run("rm /tmp/{}".format(filename))
+
+        run("mv /data/web_static/releases/{}/web_static/*"
+        " /data/web_static/releases/{}"
+        .format(filename, filename))
+
+        run("rm -rf /data/web_static/releases/{}/web_static"
+        .format(filename))
+
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(filename))
+        return True
+    except:
+        return False
